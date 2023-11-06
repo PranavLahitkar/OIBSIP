fa87adbfb450244a2e04c8b4fd02f301e2216bf7
Pranav Arun Lahitkar <109038291+PranavLahitkar@users.noreply.github.com>
2023-11-06 22:32:04 +0530

    TASK 2;Number Guessing Game
    The system generates a random number from a given range, say 1 to 100.
    The user is prompted to enter their given number in a displayed dialogue box.
    The computer then tells if the entered number matches the guesses number or it is higher/lower than the generated number.
    The game continues under the user guessing the number.
    You can also incorporate further details as:
    
    Limiting the number of attempts.
    Adding more rounds.
    Displaying score.
    Giving points based on the number of attempts.

---
+++ number_game_OIBSIP.java
@@ -0,0 +1,62 @@
+package codsoft;
+import java.util.*;
+
+public class number_game_codsoft {
+	
+	
+	public number_game_codsoft(int ch){
+		
+		Scanner sc=new Scanner(System.in);
+		int score=0;
+		Random r=new Random();
+		int random_no=r.nextInt(100)+1;//one is added because random function will guess between 1-99 here we can take range of 101 also
+		System.out.println(random_no);
+		int remainig_ch=ch;//counting remaining chanses
+		
+		while(true) {
+		
+			System.out.println("Guess The number between (1-100=)");
+			System.out.println("You have "+remainig_ch+" chances");
+			int guess=sc.nextInt();
+			score++;
+			remainig_ch--;
+			//equals or not
+			if(score==ch) {
+			
+			System.out.println("Sorry you have reached the max attempts:(\n try again later");
+			System.out.println("The number was :\t"+random_no);
+			break;
+			
+			
+			}
+		
+			if(guess==random_no) {
+			
+				System.out.println("Congratulations your guess is correct....!");
+				System.out.println("Your score is:"+score);
+				break;
+			
+			}//greater check
+			else if(random_no > guess) {
+				System.out.println("The number is greater than your guess");
+				
+				
+			
+			}//smaller check
+			else if(random_no < guess) {
+				System.out.println("The number is smaller than your guess");
+			
+			}
+		
+	
+		}
+		
+	}
+
+	public static void main(String[] args) {
+		
+		number_game_codsoft codesoft=new number_game_codsoft(5);//passed the number of chances to constructore
+		
+	}
+
+}
